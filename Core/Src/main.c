f8f07ac8a775ac923022bc0f0d290936c0ffc320
Fazanza <92736464+Fazanza@users.noreply.github.com>
2023-04-09 01:07:12 -0400

    Added Pitch, Roll (will add Yaw tmr)
    currently adding quaternion features

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,6 +21,10 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "stdbool.h"
+
+
 #define I2C_Addr 0x50
 /* PAGE ZERO
  * At power-on Page 0 is selected, PAGE_ID register
@@ -177,6 +181,7 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -188,13 +193,55 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+/* DWT Initialization */
+uint32_t DWT_Delay_Init(void) {
+	/* Disable TRC */
+	CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk;
+	/* Enable TRC */
+	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+	/* Disable clock cycle counter */
+	DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk;
+	/* Enable clock cycle counter */
+	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
+	/* Reset the clock cycle counter value */
+	DWT->CYCCNT = 0;
+
+	/* 3 Noops */
+	__ASM volatile ("NOP");
+	__ASM volatile ("NOP");
+	__ASM volatile ("NOP");
+
+	/* Check if clock cycle counter has started */
+	if (DWT->CYCCNT) {
+		return 0;  /* Clock cycle counter started */
+	} else {
+		return 1; /* clock cycle counter not started */
+	}
+}
+
+/* Delay us function */
+__STATIC_INLINE void DWT_Delay_us(volatile uint32_t au32_microseconds) {
+	uint32_t au32_initial_ticks = DWT->CYCCNT;
+	uint32_t au32_ticks = (HAL_RCC_GetHCLKFreq() / 1000000);
+	au32_microseconds *= au32_ticks;
+	while ((DWT->CYCCNT - au32_initial_ticks) < au32_microseconds-au32_ticks);
+}
+
+/* Delay ms function */
+__STATIC_INLINE void DWT_Delay_ms(volatile uint32_t au32_microseconds) {
+	uint32_t au32_initial_ticks = DWT->CYCCNT;
+  uint32_t au32_ticks = (HAL_RCC_GetHCLKFreq() / 1000);
+  au32_microseconds *= au32_ticks;
+  while ((DWT->CYCCNT - au32_initial_ticks) < au32_microseconds-au32_ticks);
+}
 /* USER CODE END 0 */
 
 /**
@@ -226,12 +273,9 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   HAL_StatusTypeDef ret;
-  volatile int16_t xGyro = 0, yGyro = 0, zGyro = 0;
-  volatile int16_t xAcc = 0, yAcc = 0, zAcc = 0;
-  volatile int16_t xMag = 0, yMag = 0, zMag = 0;
-  volatile int32_t time = 0;
   uint8_t buf[20];
 
   // SETUP for IMU
@@ -263,7 +307,7 @@ int main(void)
 	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
 	HAL_Delay(10);
 
-	/* Set Page Number */
+	/* Set Page Number to 0 */
 	buf[0] = PAGE_ID;
 	buf[1] = 0x00;
 	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
@@ -274,14 +318,73 @@ int main(void)
 	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
 	HAL_Delay(10);
 
-	/* Set Trigger Addr */
+	/* Change Units */
+	buf[0] = UNIT_SEL;
+
+	/* Set Page Number to 1*/
+	buf[0] = PAGE_ID;
+	buf[1] = 0x01;
+	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
+
+	/* Change +/- G */
+	buf[0] = ACC_CONFIG;
+	buf[1] = 0x02;
+	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
+
+	/* Change +/- Gyro */
+	buf[0] = GYR_CONFIG_0;
+	buf[1] = 0x02;
+	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
+	/* Set Page Number to 0 */
+	buf[0] = PAGE_ID;
+	buf[1] = 0x00;
+	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
+
+	/* Set Operation Mode */
 	buf[0] = OPR_MODE;
 	buf[1] = 0x07;
 	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 2, 1000);
 	HAL_Delay(30);
 
-	buf[0] = OPR_MODE;
-	HAL_I2C_Master_Receive(&hi2c1, I2C_Addr, &buf[0], 1, 1000);
+	volatile int16_t xGyro = 0, yGyro = 0, zGyro = 0;
+	int16_t xGyro_Cal = 0, yGyro_Cal = 0, zGyro_Cal = 0;
+
+	volatile int16_t xAcc = 0, yAcc = 0, zAcc = 0;
+	int16_t Acc_Vector = 0;
+
+	int8_t set_gyro_angles = 0;
+
+	volatile int16_t xMag = 0, yMag = 0, zMag = 0;
+
+	volatile int32_t time = 0;
+
+	float angle_pitch = 0, angle_roll = 0;
+	int angle_pitch_buffer = 0, angle_roll_buffer = 0;
+	float angle_roll_acc, angle_pitch_acc;
+	float angle_pitch_output, angle_roll_output;
+
+	// Quaternion declaration
+	float quaternion[4] = {0, 0, 0, 0};
+
+	// Calibration of the sensor
+	for (int cal_index = 0; cal_index < 2000; cal_index++) {
+		buf[0] = GYR_DATA_X_LSB;
+		HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 1, 1000);
+		HAL_I2C_Master_Receive(&hi2c1, I2C_Addr, &buf[0], 6, 1000);
+		xGyro = buf[0] | (buf[1] << 8);
+		yGyro = buf[2] | (buf[3] << 8);
+		zGyro = buf[4] | (buf[5] << 8);
+		xGyro_Cal += xGyro;
+		yGyro_Cal += yGyro;
+		zGyro_Cal += zGyro;
+		// TODO: Want a 3 microsecond delay here
+		HAL_Delay(1);
+	}
+
+	// Average calibration data
+	xGyro_Cal /= 2000;
+	yGyro_Cal /= 2000;
+	zGyro_Cal /= 2000;
 
 
   /* USER CODE END 2 */
@@ -291,7 +394,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
+	/* Reading in Basic Data */
 	buf[0] = ACC_DATA_X_LSB;
 	HAL_I2C_Master_Transmit(&hi2c1, I2C_Addr, &buf[0], 1, 1000);
 	HAL_I2C_Master_Receive(&hi2c1, I2C_Addr, &buf[0], 18, 1000);
@@ -306,7 +411,84 @@ int main(void)
 	yGyro = buf[14] | (buf[15] << 8);
 	zGyro = buf[16] | (buf[17] << 8);
 
-	HAL_Delay(1);
+	// Subtract offset calibration from raw gyro_x value
+	xGyro -= xGyro_Cal;
+	yGyro -= yGyro_Cal;
+	zGyro -= zGyro_Cal;
+
+	/*
+	 *
+	 * CALCULATIONS FOR ROLL, PITCH, YAW
+	 *
+	 */
+
+	// Gyro Angle Calculations
+	// 0.0000611 = 1 / (250Hz / 65.5)
+	angle_pitch += xGyro * 0.0000611;
+	angle_roll += yGyro * 0.000611;
+
+	angle_pitch += angle_roll * sin(zGyro * 0.000001066);
+	angle_roll -= angle_pitch * sin(zGyro * 0.000001066);
+
+	// Accelerometer angle calculations
+	Acc_Vector = sqrt((xAcc * xAcc) + (yAcc * yAcc) + (zAcc * zAcc));
+	angle_pitch_acc = asin((float) yAcc / Acc_Vector) * 57.296;
+	angle_roll_acc = asin((float) xAcc / Acc_Vector) * -57.296;
+
+	// TODO: Place the MPU-6050 spirit level and note the values in the following two lines for calibration
+	angle_pitch_acc -= 0; // Accelerometer calibration value for pitch
+	angle_roll_acc -= 0; // Accelerometer calibration value for roll
+
+	if (set_gyro_angles == 1) {
+		// prev was 0.9996 and 0.0004
+		// NOTE: Seems to perform better at 0.90 and 0.1
+		angle_pitch = angle_pitch * 0.90 + angle_pitch_acc * 0.1; // Correct drift gyro pitch angle with accelerometer
+		angle_roll = angle_roll * 0.90 + angle_roll_acc * 0.1; // Correct drift gyro roll angle with accelerometer
+	} else {
+		angle_pitch = angle_pitch_acc;
+		angle_roll = angle_roll_acc;
+		HAL_Delay(1000);
+	}
+
+	// To dampen pitch and roll angles, complementary filter is applied
+	angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;
+	angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;
+
+	/*
+	 *
+	 * END OF CALCULATIONS FOR ROLL, PITCH, YAW
+	 *
+	 */
+
+	/*
+	 *
+	 * CALCULATIONS FOR VELOCITY
+	 *
+	 */
+	/* Some Notes:
+	 * x0 and x1 have same distance from epicenter
+	 * x0 = r * cos (theta) || y0 = r * sin (theta)
+	 * x1 = r * cos (theta + phi) || y1 = r * cos (theta + phi)
+	 * x1 = x0 * cos (phi) - y0 * sin (phi)
+	 * y1 = x0 * sin(phi) + y0 * cos (phi)
+	 */
+
+	if (set_gyro_angles == 1) {
+
+	} else {
+		quaternion[0] = 1;
+		quaternion[1] = xGyro / 2;
+		quaternion[2] = yGyro / 2;
+		quaternion[3] = zGyro / 2;
+	}
+
+	// Sets the boolean for the gyro angles
+	set_gyro_angles = 1;
+	printf("%f %f \n", angle_pitch_output, angle_roll_output);
+	//printf("Pitch: %f		Roll: %f \n\r", angle_pitch_output, angle_roll_output);
+	//printf("Calc Pitch: %f	Calc Roll: %f \n\r", pitch, roll);
+	HAL_Delay(5);
+	/* End of Basic Data Reading */
   }
   /* USER CODE END 3 */
 }
@@ -420,6 +602,41 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -481,6 +698,16 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
 
 /* USER CODE END 4 */
 
